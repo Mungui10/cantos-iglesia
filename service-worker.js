d7826d2a0eea5ce690ad1a2d07f500381871bf60
Mungui10 <munguiamsh10910@gmail.com>
2026-07-18 12:12:26 -0400

    Publish Himnario PWA

---
+++ service-worker.js
@@ -0,0 +1,21 @@
+const CACHE_VERSION = "himnario-pwa-v1";
+
+self.addEventListener("install", () => {
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(
+        keys
+          .filter((key) => key.startsWith("himnario-") && key !== CACHE_VERSION)
+          .map((key) => caches.delete(key))
+      )
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener("fetch", () => {
+  return;
+});
